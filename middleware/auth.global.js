0c525a5e46755e9ec1b08a64603e7ec6de280339
fabian1606 <fabian@rafreider.com>
2024-07-03 00:22:33 +0200

    fix sincronous middleware

--- middleware/auth.global.js
+++ middleware/auth.global.js
@@ -1,26 +1,19 @@
 import { userStore } from '~/stores/auth.js';
 
-export default defineNuxtRouteMiddleware
-    ((to, from) => {
-        const runtimeConfig = useRuntimeConfig();
-        const authStore = userStore();
-        const redirectUrl = useRequestURL().protocol+"//"+useRequestURL().host;
-        console.log(redirectUrl);
-        if (!authStore.loggedIn) {
-            authStore.checkAuth(to.query.code)
-                .then((res) => {
-                    console.log('checkAuth', res);
-                    if (res) {
-                        return(true);
-                    } else {
-                        console.log('auth redirect');
-                        const authUrl = `https://www.figma.com/oauth?redirect_uri=${redirectUrl}&response_type=code&state=8TCNJE36S3MyBee5wFLxvwVL&scope=file_read&client_id=${runtimeConfig.public.FIGMA_CLIENT_ID}`;
-                        // window.location.href = authUrl;
-                        //redirect to figma auth
-                        authStore.loggedIn = false;
-                        return navigateTo(authUrl, { external: true });
-                    }
-                });
+export default defineNuxtRouteMiddleware(async (to, from) => {
+    const runtimeConfig = useRuntimeConfig();
+    const authStore = userStore();
+    const redirectUrl = useRequestURL().protocol + "://" + useRequestURL().host;
+
+    if (!authStore.loggedIn) {
+        const res = await authStore.checkAuth(to.query.code);
+        if (res) {
+            return true;
+        } else {
+            const authUrl = `https://www.figma.com/oauth?redirect_uri=${redirectUrl}&response_type=code&state=8TCNJE36S3MyBee5wFLxvwVL&scope=file_read&client_id=${runtimeConfig.public.FIGMA_CLIENT_ID}`;
+            authStore.loggedIn = false;
+            return navigateTo(authUrl, { external: true });
         }
-        return (true);
-    })
+    }
+    return true;
+});
